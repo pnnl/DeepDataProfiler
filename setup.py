30df04343d16b60ffa4b2f8a7e950c71463b13b4
Brenda Praggastis <brenda.praggastis@pnnl.gov>
2020-05-19 10:12:19 -0700

    updating version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os.path
 
-__version__ = '0.2'
+__version__ = '0.3'
 
 if sys.version_info < (3,6):
     sys.exit('DeepDataProfiler requires Python 3.6 o later')
@@ -19,16 +19,16 @@ setup(
     context of a specific dataset. The library was inspired by the work of Qiu, et al. in Adversarial Defense Through
     Network Profiling Based Path Extraction (2019), arXiv:1904.08089.
 
-    The current version is preliminary. We are actively testing and would be grateful 
-    for comments and suggestions. Expect changes in both class names and methods as 
+    The current version is preliminary. We are actively testing and would be grateful
+    for comments and suggestions. Expect changes in both class names and methods as
     many of the requirements demanded of the library are worked out.
-    ''',  
-    install_requires=['torch>=0.4.1','numpy>=1.14.3'],  
+    ''',
+    install_requires=['torch>=0.4.1','numpy>=1.14.3'],
     extras_require={
         'testing':['pytest>=4.0'],
         'documentation':['sphinx>=1.8.2','nb2plots>=0.6','sphinx-rtd-theme>=0.4.2'],
         'all':['sphinx>=1.8.2','nb2plots>=0.6','sphinx-rtd-theme>=0.4.2','pytest>=4.0',
-                'torch>=0.4.1','pillow>=5.2.0','torchvision>=0.2.1','jupyter>=1.0.0','numpy>=1.14.3'],        
+                'torch>=0.4.1','pillow>=5.2.0','torchvision>=0.2.1','jupyter>=1.0.0','numpy>=1.14.3'],
     }
 )
 
