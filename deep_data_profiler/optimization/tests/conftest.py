9e6b571e3c19863b75a247a9a05b517fd4d298b3
Brown, Davis R <davis.brown@pnnl.gov>
2022-01-20 12:34:27 -0800

    adds tests

--- deep_data_profiler/optimization/tests/conftest.py
+++ deep_data_profiler/optimization/tests/conftest.py
@@ -2,12 +2,16 @@ import pytest
 import torch
 from torchvision.models import resnet18
 import deep_data_profiler as ddp
+from deep_data_profiler.optimization import project_svd
 
 
 class VisionExample:
     def __init__(self):
         torch.manual_seed(0)
         self.model = resnet18(pretrained=True).to("cpu").eval()
+        self.profiler = ddp.ChannelProfiler(self.model)
+        self.svd_projection = project_svd(self.profiler)
+        self.hooked_model = ddp.utils.TorchHook(self.model)
 
 
 @pytest.fixture
